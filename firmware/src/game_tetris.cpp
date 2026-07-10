e2e3e8c39b5847e03ce27916235dc52d5c2b5faa
Milind Singh <milindsingh486@gmail.com>
2026-07-10 13:13:14 +0530

    optmimized tetris and will be adding screensaver

--- firmware/src/game_tetris.cpp
+++ firmware/src/game_tetris.cpp
@@ -11,43 +11,62 @@ static CRGB pieceColor = CRGB::Cyan;
 static int currentPiece[4][4];
 
 const int TETROMINOES[7][4][4] = {
-    {{0,0,0,0}
-     {1,1,1,1}
-     {0,0,0,0}
+    {{0,0,0,0},
+     {1,1,1,1},
+     {0,0,0,0},
      {0,0,0,0}},
 
-    {{1,0,0,0}
-     {1,1,1,0}
-     {0,0,0,0}
+    {{1,0,0,0},
+     {1,1,1,0},
+     {0,0,0,0},
      {0,0,0,0}},
 
-    {{0,0,1,0}
-     {1,1,1,0}
-     {0,0,0,0}
+    {{0,0,1,0},
+     {1,1,1,0},
+     {0,0,0,0},
      {0,0,0,0}},
     
-    {{0,1,1,0}
-     {0,1,1,0}
-     {0,0,0,0}
+    {{0,1,1,0},
+     {0,1,1,0},
+     {0,0,0,0},
      {0,0,0,0}},
 
-    {{0,1,1,0}
-     {1,1,0,0}
-     {0,0,0,0}
+    {{0,1,1,0},
+     {1,1,0,0},
+     {0,0,0,0},
      {0,0,0,0}},
 
-    {{0,1,0,0}
-     {1,1,1,0}
-     {0,0,0,0}
+    {{0,1,0,0},
+     {1,1,1,0},
+     {0,0,0,0},
      {0,0,0,0}},
 
-    {{1,1,0,0}
-     {0,1,1,0}
-     {0,0,0,0}
+    {{1,1,0,0},
+     {0,1,1,0},
+     {0,0,0,0},
      {0,0,0,0}}
 };
 
-void SpawnNewPiece
+void SpawnNewPiece() {
+    int shapeID = random(7);
+
+    for (int y=0; y<4; y++) {
+        for (int x=0; x<4; x++) {
+            currentPiece[y][x] = TETROMINOES[shapeID][y][x];
+        }
+    }
+
+    if (shapeID == 0) pieceColor = CRGB::Cyan;
+    if (shapeID == 1) pieceColor = CRGB::Blue;
+    if (shapeID == 2) pieceColor = CRGB::Orange;
+    if (shapeID == 3) pieceColor = CRGB::Yellow;
+    if (shapeID == 4) pieceColor = CRGB::Green;
+    if (shapeID == 5) pieceColor = CRGB::Purple;
+    if (shapeID == 6) pieceColor = CRGB::Red;
+
+    pieceX = 3;
+    pieceY = 0;
+}
 
 void ResetTetris() {
     for (int y = 0; y < 10; y++) {
@@ -59,6 +78,64 @@ void ResetTetris() {
     pieceY = 0;
     gameActive = true;
 }
+bool CheckCollision(int nextX, int nextY, int piece[4][4]) {
+    for (int y = 0; y < 4; y++) {
+        for (int x = 0; x < 4; x++) {
+            if (piece[y][x] == 1) {
+                int boardX = nextX + x;
+                int boardY = nextY + y;
+
+                if (boardX < 0 || boardX > 9 || boardY > 9) return true; 
+                if (boardY >=0) {
+                    if (board[boardY][boardX] != CRGB::Black) return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+void RotatePiece() {
+    int temp[4][4];
+
+    for (int y = 0; y < 4; y++) {
+        for (int x = 0; x < 4; x++) {
+            temp[x][3 - y] = currentPiece[y][x];
+        }
+    }
+
+    if (!CheckCollision(pieceX, pieceY, temp)) {
+        for (int y = 0; y < 4; y++) {
+            for (int x = 0; x < 4; x++) {
+                currentPiece[y][x] = temp[y][x];
+            }
+        }
+    }
+}
+
+void ClearLines() {
+    for (int y = 9; y >= 0; y--) {
+        bool full = true;
+        for (int x = 0; x < 10; x++) {
+            if (board[y][x] == CRGB::Black) {
+                full = false;
+                break;
+            }
+        }
+
+        if (full) {
+            for (int shiftY = y; shiftY > 0; shiftY--) {
+                for (int x = 0; x < 10; x++) {
+                    board[shiftY][x] = board[shiftY - 1][x];
+                }
+            }
+            for (int x = 0; x < 10; x++) {
+                board[0][x] = CRGB::Black;
+            }
+            y++; 
+        }
+    }
+}
 
 void RunTetris(int input) {
     static unsigned long lastGravity = 0;
@@ -68,22 +145,37 @@ void RunTetris(int input) {
         return;
     }
 
-    if (input == PIN_JOY_LEFT) pieceX--;
-    if (input == PIN_JOY_RIGHT) pieceX++;
+    if (input == PIN_JOY_LEFT) {
+        if (!CheckCollision(pieceX - 1, pieceY, currentPiece)) pieceX--;
+    }
+    if (input == PIN_JOY_RIGHT) {
+        if (!CheckCollision(pieceX + 1, pieceY, currentPiece)) pieceX++;
+    }
+    if (input == PIN_JOY_DOWN) {
+        if (!CheckCollision(pieceX, pieceY + 1, currentPiece)) pieceY++;
+    }
+    if (input == PIN_BTN_A) {
+        RotatePiece();
+    }
+
 
     if (millis() - lastGravity > 500) {
         lastGravity = millis();
-        pieceY++;
 
-        if (pieceY > 8) {
-            for (int y=0; y<2; y++) {
-                for (int x=0; x<2; x++) {
-                    if (currentPiece[y][x] == 1) board[pieceY + y][pieceX + x] = pieceColor;
+        if (!CheckCollision(pieceX, pieceY + 1, currentPiece)) {
+            pieceY++;
+        } else {
+            for (int y = 0; y < 4; y++) {
+                for (int x = 0; x < 4; x++) {
+                    if (currentPiece[y][x] == 1) {
+                        if (pieceY + y >= 0) {
+                            board[pieceY + y][pieceX + x] = pieceColor;
+                        }
+                    }
                 }
             }
-
-            pieceX = 4;
-            pieceY = 0;
+            ClearLines();
+            SpawnNewPiece();
         }
     }
 
@@ -93,8 +185,8 @@ void RunTetris(int input) {
         }
     }
 
-    for (int y = 0; y < 2; y++) {
-        for (int x = 0; x < 2; x++) {
+    for (int y = 0; y < 4; y++) {
+        for (int x = 0; x < 4; x++) {
             if (currentPiece[y][x] == 1) DrawPixel(pieceX + x, pieceY + y, pieceColor);
         }
     }
